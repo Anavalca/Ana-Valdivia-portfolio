e56b5c1c500a2dba014b8f0793e875f132a0f834
AnaValca <avc92j@gmail.com>
2020-05-21 13:44:23 +0200

    add icons rrss

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import IconMail from '../icons/mail.png';
+import IconLinkedin from '../icons/linkedin.png';
+import IconGithub from '../icons/github.png';
+import IconTwitter from '../icons/twitter.png';
 
 const Main = (props) => {
     console.log(props)
@@ -23,6 +27,20 @@ const Main = (props) => {
                     </div>
                 </div>
                 <div className='subtitle'> I´m Ana Valdivia. Frontend Developer</div>
+            </div>
+            <div className='iconsRRSS'>
+                <a title='mail' href="mailto:avc92j@gmail.com" target='blank'>
+                    <img className='iconMail' alt='icon Email' src={IconMail} />
+                </a>
+                <a title='linkedin' href='https://www.linkedin.com/in/anavaldiviacano/' target='blank'>
+                    <img className='iconLinkedin' alt='icon linkedin' src={IconLinkedin} />
+                </a>
+                <a title='github' href='https://github.com/Anavalca' target='blank'>
+                    <img className='iconGithub' alt='icon github' src={IconGithub} />
+                </a>
+                <a title='twitter' href='https://twitter.com/Anita_amarillo' target='blank'>
+                    <img className='iconTwitter' alt='icon twitter' src={IconTwitter} />
+                </a>
             </div>
             <span class="scrollIcon">
                 {/* <a href="#"> */}
