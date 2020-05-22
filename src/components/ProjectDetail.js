6d2a8b6b6a78fbfdb2ebe83abf71202a54a74afe
AnaValca <avc92j@gmail.com>
2020-05-22 15:20:13 +0200

    add project detail page structure

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import IconGithub from '../icons/github.png';
 
 const ProjectDetails = (props) => {
-    console.log(props)
-  return(
+  console.log(props)
+  return (
     <div className="projectDetailPage">
-      <p>{props.project.name}</p>
-          {/* <ul className='genres'>
-            {props.show.genres.map((genre, index) =>
+      <div className='projectDetailContainer'>
+      <div className='mockupProjectP'>
+          <img alt='mockup' src={props.project.mockup}/>
+        </div>
+        <div className='infoProjectDetailContainer'>
+          <p className='titleProject'>{props.project.name}</p>
+          <ul className='tags'>
+            {props.project.tags.map((tag, index) =>
               <span key={index}>
-                  {genre}
-              </span>                        
+                {tag}
+              </span>
             )}
-          </ul> */}
+          </ul>
+          <p>{props.project.description}</p>
+          <div className='linkContainer'>
+            <button className='buttonShowWeb' title='Web Page' href={props.project.url}>Web</button>
+            <a title='Github' href={props.project.githubUrl} target='blank'>
+              <img className='iconGithub' alt='icon github' src={IconGithub} />
+            </a>
+          </div>
+        </div>
+        
+      </div>
     </div>
   )
 }
