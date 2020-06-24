a565b545cc77a5346ba84a6a73a6f90fbd26dd78
AnaValca <avc92j@gmail.com>
2020-06-24 11:38:11 +0200

    add docs

---
+++ docs/precache-manifest.7087943db41b8825395afe1b2700dea7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bf2393b5e4750081de84cfcfc8252e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82ac63bda28b738098f7",
+    "url": "./static/css/main.f0dbbb57.chunk.css"
+  },
+  {
+    "revision": "39ecbd1646a87ec1db0f",
+    "url": "./static/js/2.dbe93d3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dbe93d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ac63bda28b738098f7",
+    "url": "./static/js/main.a892c844.chunk.js"
+  },
+  {
+    "revision": "a062e1f0e1f924f559d3",
+    "url": "./static/js/runtime-main.2afa580b.js"
+  },
+  {
+    "revision": "d0e97d60beb60587daf0a9a793b640dc",
+    "url": "./static/media/CV_Ana_Valdivia.d0e97d60.pdf"
+  },
+  {
+    "revision": "e73c9ba44cba7a5157c690892bd15aec",
+    "url": "./static/media/carrete_Pc.e73c9ba4.jpg"
+  },
+  {
+    "revision": "cf2ea458f60b39de8644c75bc8090321",
+    "url": "./static/media/carrete_Tablet.cf2ea458.jpg"
+  },
+  {
+    "revision": "2543150f40c758d2b0bd18e86a4dcedf",
+    "url": "./static/media/carrete_m.2543150f.jpg"
+  },
+  {
+    "revision": "6ace7a0d03ef1d7ab939a0cb6feda0c6",
+    "url": "./static/media/cassete.6ace7a0d.jpg"
+  },
+  {
+    "revision": "a9d0171b37cbd4f80da005a7b010465e",
+    "url": "./static/media/cassete_m2.a9d0171b.jpg"
+  },
+  {
+    "revision": "0250bd43033c02698dd7da8e22db4990",
+    "url": "./static/media/coronavirus.0250bd43.jpg"
+  },
+  {
+    "revision": "37e697d7a385f99b693708bab458ed6a",
+    "url": "./static/media/github.37e697d7.png"
+  },
+  {
+    "revision": "6c368d439b083a82a29123468d00c863",
+    "url": "./static/media/mail.6c368d43.png"
+  },
+  {
+    "revision": "d555dbe4b049bb195fc341bd14fd85d6",
+    "url": "./static/media/malaga.d555dbe4.jpg"
+  },
+  {
+    "revision": "b3b5294e62ab562f6c4ed5538affeeda",
+    "url": "./static/media/mother.b3b5294e.jpg"
+  },
+  {
+    "revision": "e3e240272dda02bda721141052cf41f4",
+    "url": "./static/media/puzzle.e3e24027.jpg"
+  },
+  {
+    "revision": "b71da660afb2127aaefac19ff15fbceb",
+    "url": "./static/media/rick.b71da660.jpg"
+  },
+  {
+    "revision": "06e90b3e209114f5aef92049da7599ae",
+    "url": "./static/media/twitter.06e90b3e.png"
+  },
+  {
+    "revision": "2f36b9d2d878d3e290bbb02a0dd26e78",
+    "url": "./static/media/web.2f36b9d2.png"
+  }
+]);
